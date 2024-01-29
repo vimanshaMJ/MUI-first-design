43b165af46d27897ba3f1a72b93e503785541e7d
vimansha <vimanshamadushani88@gmail.com>
2024-01-30 00:29:26 +0530

    Stack component and styles

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Box, Container, Stack } from "@mui/material";
 import "./App.css";
 import Feed from "./Components/Feed";
 import Rightbar from "./Components/Rightbar";
@@ -5,11 +6,14 @@ import Sidebar from "./Components/Sidebar";
 
 function App() {
   return (
-    <div>
-      <Sidebar />
-      <Feed />
-      <Rightbar />
-    </div>
+    <Box>
+      {/* Navbar */}
+      <Stack direction="row" spacing={5} justifyContent="space-evenly">
+        <Sidebar />
+        <Feed />
+        <Rightbar />
+      </Stack>
+    </Box>
   );
 }
 
